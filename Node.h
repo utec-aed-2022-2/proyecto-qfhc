68bb496531d97438a3721ec195b51f226e801470
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2022-10-17 16:47:23 -0500

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,31 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct Node {
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+
+    Node(){
+        //data;
+        next = nullptr;
+        prev = nullptr;
+    }
+
+    Node(T value){
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+
+    void killSelf(){
+        delete this;
+    }
+};
+
+#endif
